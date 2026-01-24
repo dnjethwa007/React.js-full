7cea52bf95283eff92fb2a101d6c14591af9c8f8
dnjethwa007 <jethwadeven70@gmail.com>
2026-01-24 18:07:12 +0530

    crete first react component

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./Chai"
+
+function App() {
+  
+  return (
+    <>
+
+      <h1>Hello deven</h1>
+      <Chai />
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,9 @@
+function Chai(){
+    return (
+        <>
+            <h1>Chai Component</h1>
+            <p>This is the Chai component content.</p>
+        </>
+    )
+}
+export default Chai;
